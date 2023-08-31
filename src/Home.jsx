06d7587489d4938e3ead231964ce9c5159ee09fa
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-08-31 17:15:50 +0200

    add hero

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,22 @@
+import { Link } from 'react-router-dom'
+import './Home.css'
+import image from './finance.svg'
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <main className="home">
+      <section className="home__section">
+        <p className="home__text">
+          Welcome to Invest.io - your financial compass to monitor your
+          investment portfolio with ease and precision.
+        </p>
+        <Link to="/shares" className="home__cta">
+          Check Now!
+        </Link>
+      </section>
+      <section className="home__section">
+        <img src={image} alt="man showing investments on monitor" />
+      </section>
+    </main>
+  )
 }
 export default Home
