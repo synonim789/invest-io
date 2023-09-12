ab464b534edd6a3af3640c3ff6ec81fa4c4724f8
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-09-12 19:06:26 +0200

    add mobile navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,20 @@
 import { Link, NavLink } from 'react-router-dom'
 import './Navbar.css'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <nav className="navbar">
       <Link to="/" className="navbar__logo">
         <h1>Invest.io</h1>
       </Link>
-      <ul className="navbar__list">
+      <ul
+        className={
+          'navbar__list' + (open === true ? ' navbar__list--active' : '')
+        }
+      >
         <li className="navbar__item">
           <NavLink
             to="/shares"
@@ -39,7 +46,17 @@ const Navbar = () => {
           </NavLink>
         </li>
       </ul>
-      <div className="navbar__hamburger"></div>
+      <div
+        className={
+          'navbar__hamburger' +
+          (open === true ? ' navbar__hamburger--active' : '')
+        }
+        onClick={() => setOpen(!open)}
+      >
+        <div className="navbar__hamburger-bar"></div>
+        <div className="navbar__hamburger-bar"></div>
+        <div className="navbar__hamburger-bar"></div>
+      </div>
     </nav>
   )
 }
