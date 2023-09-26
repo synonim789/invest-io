a267e025d69d0ee836e93d9a5d13ede218e8eab6
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-09-26 18:59:06 +0200

    add crypto inputs

--- src/Crypto.jsx
+++ src/Crypto.jsx
@@ -1,4 +1,30 @@
 const Crypto = () => {
-  return <div>Crypto</div>
+  return (
+    <div className="crypto">
+      <h1 className="crypto__title">Crypto</h1>
+      <div className="crpyto__inputs">
+        <input type="number" placeholder="amount" className="crypto__input" />
+        <input
+          type="list"
+          placeholder="from"
+          list="crypto__list-1"
+          className="crypto__list"
+        />
+        <datalist id="crypto__list-1">
+          <option value="USD"></option>
+        </datalist>
+        <input
+          type="list"
+          placeholder="to"
+          list="crypto__list-2"
+          className="crypto__list"
+        />
+        <datalist id="crypto__list-2">
+          <option value="PLN"></option>
+        </datalist>
+        <button className="crypto__btn">Calculate</button>
+      </div>
+    </div>
+  )
 }
 export default Crypto
