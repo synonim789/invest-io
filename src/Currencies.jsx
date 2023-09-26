7d9fbea17e846987cd5ad3b1dff977eb3c4c5e4b
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-09-26 18:38:29 +0200

    add currencies inputs

--- src/Currencies.jsx
+++ src/Currencies.jsx
@@ -1,4 +1,34 @@
 const Currencies = () => {
-  return <div>Currencies</div>
+  return (
+    <div className="currrencies">
+      <h1 className="currenices__title">Currencies</h1>
+      <div className="currencies__inputs">
+        <input
+          type="number"
+          className="currencies__input"
+          placeholder="amount"
+        />
+        <input
+          type="list"
+          className="currencies__list"
+          list="currencies__list-1"
+          placeholder="from"
+        />
+        <datalist id="currencies__list-1">
+          <option value="USD"></option>
+        </datalist>
+        <input
+          type="list"
+          className="currencies__list"
+          placeholder="to"
+          list="currencies__list-2"
+        />
+        <datalist id="currencies__list-2">
+          <option value="PLN"></option>
+        </datalist>
+        <button className="currencies__btn">Calculate</button>
+      </div>
+    </div>
+  )
 }
 export default Currencies
