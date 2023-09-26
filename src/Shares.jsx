fe944427d4847c155278ace39769836dc70072a7
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-09-26 19:20:00 +0200

    add stock inputs

--- src/Shares.jsx
+++ src/Shares.jsx
@@ -1,4 +1,25 @@
 const Shares = () => {
-  return <div>Shares</div>
+  return (
+    <div className="stock">
+      <h1 className="stock__title">Stock</h1>
+      <div className="stock__inputs">
+        <input
+          type="number"
+          className="stock__inputs"
+          placeholder="shares amount"
+        />
+        <input
+          type="list"
+          list="stock__list-1"
+          className="stock__list"
+          placeholder="company"
+        />
+        <datalist id="stock__list-1">
+          <option value="Apple"></option>
+        </datalist>
+        <button className="stock__btn">calculate</button>
+      </div>
+    </div>
+  )
 }
 export default Shares
