407232cb0b1510576a10c5a7a92ca465b14f3cd5
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-07-02 18:11:05 +0200

    update tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  distDir: './dist',
 }
 
 export default nextConfig
